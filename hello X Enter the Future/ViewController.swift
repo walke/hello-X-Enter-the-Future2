8145abaca2c26a45513b1f790fde7fa40da05c58
Jegor Sushko <diesou9@gmail.com>
2021-04-21 16:35:18 +0200

    Initial Commit

---
+++ hello X Enter the Future/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hello X Enter the Future
+//
+//  Created by Tamara Sushko on 2021-04-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hello X Enter the FutureTests/hello_X_Enter_the_FutureTests.swift
@@ -0,0 +1,33 @@
+//
+//  hello_X_Enter_the_FutureTests.swift
+//  hello X Enter the FutureTests
+//
+//  Created by Tamara Sushko on 2021-04-21.
+//
+
+import XCTest
+@testable import hello_X_Enter_the_Future
+
+class hello_X_Enter_the_FutureTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
